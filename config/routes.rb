a6178a7e2c7b81cf2355bea281c6606928900dfd
ctw <ctw.3@163.com>
2020-05-02 11:48:14 +0800

    修复

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   resources :products do
     collection do
       get 'label'
-      get 'label/LRD2100' , to: 'products#label2100'
-      get 'label/LRD6300' , to: 'products#label6300'
-      get 'label/LRD8200' , to: 'products#label8200'
-      get 'label/LionEye' , to: 'products#lioneye'
+      get 'label/label2100' , to: 'products#label2100'
+      get 'label/label6300' , to: 'products#label6300'
+      get 'label/label8200' , to: 'products#label8200'
+      get 'label/lioneye'   , to: 'products#lioneye'
 
       get 'eddy'
       get 'eddy/ecl202' , to: 'products#ecl202'
